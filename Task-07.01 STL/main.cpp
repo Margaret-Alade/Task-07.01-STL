7718ca338d514aeae3605498bb44ff36a5496253
Margaret Alade <alade.margaret@yandex.ru>
2024-02-09 19:25:10 +0300

    Initial commit

--- Task-07.01 STL/main.cpp
+++ Task-07.01 STL/main.cpp
@@ -6,9 +6,31 @@
 //
 
 #include <iostream>
+#include <set>
+#include <map>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "[IN]: ";
+    std::string str;
+    std::cin >> str;
+    std::cout << "[OUT]: " << "\n";
+    std::multiset <char> m_set_ch;
+    std::set <char> set_ch;
+    std::multimap <int, char, std::greater <int>> m_map;
+    for (int i = 0; i < str.size(); i ++) {
+        m_set_ch.insert(str[i]);
+        set_ch.insert(str[i]);
+    }
+    
+    for (const auto &elem : m_set_ch) {
+        if (set_ch.find(elem) != set_ch.end()) {
+            int num = static_cast<int>(m_set_ch.count(elem));
+            m_map.insert (std::make_pair (num, elem));
+            set_ch.erase(elem);
+        }
+    }
+    for (const auto &[a,b] : m_map) {
+        std::cout << b << ": " << a << "\n";
+    }
     return 0;
 }
